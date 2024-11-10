0c0f21dc54338181f7bde1c5255c6da77e7d137d
seif34ml <seifammar1125@gmail.com>
2024-11-11 01:02:46 +0200

    add div to fit design

--- src/ProjectLists.js
+++ src/ProjectLists.js
@@ -5,9 +5,9 @@ import AddProjectForm from './AddProjectForm';
 export default function ProjectsSidebar  ({ projects, onProjectSelect, selectedProject })  {
   return (
     <div className="projects-sidebar">
-      <h2>Projects</h2>
-      
-      <ul className="project-list">
+      <div>
+        <h2>Projects</h2>
+        <ul className="project-list">
         {projects.map((project) => (
           <ProjectItem
           key={project.id}
@@ -19,6 +19,10 @@ export default function ProjectsSidebar  ({ projects, onProjectSelect, selectedP
         />
         ))}
       </ul>
+      </div>
+      
+      
+      
 
      <AddProjectForm onAddProject={()=>{}} />   {/*onAddProject */}
     
